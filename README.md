1c922864fb3c7511dd352d944dcdc83c588df792
Zhongyi Dai <zhongyi.dai@advance.ai>
2018-11-12 22:28:41 +0800

    Add zipkin-dependencies.

---
+++ README.md
@@ -0,0 +1,5 @@
+# docker-zipkin-dependencies
+
+Dockerfile [cloud-ready/docker-zipkin-dependencies on Github](https://github.com/cloud-ready/docker-zipkin-dependencies)
+
+[cloudready/zipkin-dependencies on Docker Hub](https://hub.docker.com/r/cloudready/zipkin-dependencies/)
---
+++ docker/entrypoint.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -e
+
+export STORAGE_TYPE="${STORAGE_TYPE:-elasticsearch}"
+export ES_HOSTS="${ES_HOSTS:-http://127.0.0.1:9200}"
+
+LOG_PATH="/data/logs"
+
+JAVA_OPTS="${JAVA_OPTS} -XX:+UseG1GC"
+
+JAVA_OPTS="${JAVA_OPTS} -XX:+HeapDumpOnOutOfMemoryError -XX:-OmitStackTraceInFastThrow"
+
+# http://stackoverflow.com/questions/137212/how-to-solve-performance-problem-with-java-securerandom
+JAVA_OPTS="${JAVA_OPTS} -Djava.security.egd=file:/dev/./urandom"
+
+# crash dump & heap dump
+JAVA_OPTS="${JAVA_OPTS} -XX:ErrorFile=/tmp/jvm-crash-zipkin-dependencies.log -XX:HeapDumpPath=/tmp"
+
+JAVA_OPTS="${JAVA_OPTS} -Xloggc:${LOG_PATH}/jvmgc.log -XX:+PrintGCDetails -XX:+PrintGCDateStamps -XX:+PrintGCApplicationConcurrentTime -XX:+PrintGCApplicationStoppedTime"
+JAVA_OPTS="${JAVA_OPTS} -Dfile.encoding=UTF-8"
+export JAVA_TOOL_OPTIONS="${JAVA_TOOL_OPTIONS} ${JAVA_OPTS}"
+
+exec "$@"
