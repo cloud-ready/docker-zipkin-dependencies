a635ac60c287ba5fde1e2193a9ecba8a0e5ce60a
Zhongyi Dai <zhongyi.dai@advance.ai>
2018-11-16 14:17:57 +0800

    append log

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,7 +2,7 @@
 set -ex
 
 CRONTAB_EXPRESSION=${CRONTAB_EXPRESSION:-* */1 * * *}
-CRONTAB_SCRIPT="bash /zipkin-dependencies.sh > /data/logs/cron.log 2>&1"
+CRONTAB_SCRIPT="bash /zipkin-dependencies.sh >> /data/logs/cron.log 2>&1"
 echo "${CRONTAB_EXPRESSION} ${CRONTAB_SCRIPT}" > /data/zipkin-dependencies-cron
 
 crontab /data/zipkin-dependencies-cron
